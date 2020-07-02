c7a9bac6438b9c22445cd438d4adb611b12bf891
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2020-07-02 00:28:25 -0700

    use read and write volatile

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -1,28 +1,61 @@
 #![no_main]
 #![no_std]
 
+use core::ptr;
+
 #[allow(unused_imports)]
 use aux7::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    aux7::init();
+    let mut itm = aux7::init().0;
 
     unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
+        // A magic addresses!
+        const GPIOE_BSRR: u32 = 0x4800_1018;
+        const GPIOE_ODR: u32 = 0x4800_1014;
+
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = 0x{:04x}",
+            ptr::read_volatile(GPIOE_ODR as *mut u32)
+        );
 
         // Turn on the "North" LED (red)
-        *(GPIOE_BSRR as *mut u32) = 1 << 9;
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
+
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = 0x{:04x}",
+            ptr::read_volatile(GPIOE_ODR as *mut u32)
+        );
 
         // Turn on the "East" LED (green)
-        *(GPIOE_BSRR as *mut u32) = 1 << 11;
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
+
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = 0x{:04x}",
+            ptr::read_volatile(GPIOE_ODR as *mut u32)
+        );
 
         // Turn off the "North" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
+
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = 0x{:04x}",
+            ptr::read_volatile(GPIOE_ODR as *mut u32)
+        );
 
         // Turn off the "East" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
+
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = 0x{:04x}",
+            ptr::read_volatile(GPIOE_ODR as *mut u32)
+        );
     }
 
     loop {}
