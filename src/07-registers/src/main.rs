77f1f896a6dbaecde013b16ff097458702691ea2
Yerkebulan Tulibergenov <yerkebulan@gmail.com>
2020-07-02 00:46:24 -0700

    chapter 7. use type safe API

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -1,62 +1,24 @@
 #![no_main]
 #![no_std]
 
-use core::ptr;
-
 #[allow(unused_imports)]
 use aux7::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    let mut itm = aux7::init().0;
+    let gpioe = aux7::init().1;
 
-    unsafe {
-        // A magic addresses!
-        const GPIOE_BSRR: u32 = 0x4800_1018;
-        const GPIOE_ODR: u32 = 0x4800_1014;
+    // Turn on the North LED
+    gpioe.bsrr.write(|w| w.bs9().set_bit());
 
-        iprintln!(
-            &mut itm.stim[0],
-            "ODR = 0x{:04x}",
-            ptr::read_volatile(GPIOE_ODR as *mut u32)
-        );
+    // Turn on the East LED
+    gpioe.bsrr.write(|w| w.bs11().set_bit());
 
-        // Turn on the "North" LED (red)
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
+    // Turn off the North LED
+    gpioe.bsrr.write(|w| w.br9().set_bit());
 
-        iprintln!(
-            &mut itm.stim[0],
-            "ODR = 0x{:04x}",
-            ptr::read_volatile(GPIOE_ODR as *mut u32)
-        );
-
-        // Turn on the "East" LED (green)
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
-
-        iprintln!(
-            &mut itm.stim[0],
-            "ODR = 0x{:04x}",
-            ptr::read_volatile(GPIOE_ODR as *mut u32)
-        );
-
-        // Turn off the "North" LED
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
-
-        iprintln!(
-            &mut itm.stim[0],
-            "ODR = 0x{:04x}",
-            ptr::read_volatile(GPIOE_ODR as *mut u32)
-        );
-
-        // Turn off the "East" LED
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
-
-        iprintln!(
-            &mut itm.stim[0],
-            "ODR = 0x{:04x}",
-            ptr::read_volatile(GPIOE_ODR as *mut u32)
-        );
-    }
+    // Turn off the East LED
+    gpioe.bsrr.write(|w| w.br11().set_bit());
 
     loop {}
 }
